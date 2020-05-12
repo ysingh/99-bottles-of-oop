29d5a7a62501ed9c357fbeef4c931ad28165ede9
ysingh <ysingh0923@gmail.com>
2020-05-12 13:27:00 -0400

    Solution to Initial Exercise: Appendix B

---
+++ lib/bottles.rb
@@ -0,0 +1,40 @@
+class Bottles
+  def verse(bottles)
+    "#{firstSentence(bottles)}#{secondSentence(bottles - 1)}"
+  end
+
+  def verses(start, fin)
+    verses = []
+    start.downto(fin) do |i|
+      verses << verse(i)
+    end
+    verses.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  private
+
+  def bottle(bottles)
+    if (bottles == 0)
+      return "no more bottles"
+    elsif (bottles == 1)
+      return "#{bottles} bottle"
+    end
+    return "#{bottles} bottles"
+  end
+
+  def firstSentence(bottles)
+    bottleStr = bottle(bottles)
+    "#{bottleStr.capitalize} of beer on the wall, #{bottleStr} of beer.\n"
+  end
+
+  def secondSentence(bottles)
+    if (bottles == -1)
+      return "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+    "Take #{bottles == 0 ? 'it' : 'one'} down and pass it around, #{bottle(bottles)} of beer on the wall.\n"
+  end
+end
